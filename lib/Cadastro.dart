d276fa41b90d56f2b244ada80e9870c5d6333ca3
Bruno Tardio <brunotardiosi@gmail.com>
2020-04-02 16:28:08 -0300

    Validar cadastro de usuario

--- lib/Cadastro.dart
+++ lib/Cadastro.dart
@@ -6,6 +6,58 @@ class Cadastro extends StatefulWidget {
 }
 
 class _CadastroState extends State<Cadastro> {
+
+  //Controladores
+  TextEditingController _controllerNome = TextEditingController();
+  TextEditingController _controllerEmail = TextEditingController();
+  TextEditingController _controllerSenha = TextEditingController();
+  String _mensagemErro = "";
+
+  _validarCampos(){
+
+    //Recupera dados dos campos
+    String nome = _controllerNome.text;
+    String email = _controllerEmail.text;
+    String senha = _controllerSenha.text;
+
+    if( nome.isNotEmpty ){
+
+      if( email.isNotEmpty && email.contains("@") ){
+
+        if( senha.isNotEmpty ){
+
+          setState(() {
+            _mensagemErro = "";
+          });
+          _cadastrarUsuario();
+
+
+        }else{
+          setState(() {
+            _mensagemErro = "Preencha a senha!";
+          });
+        }
+
+      }else{
+        setState(() {
+          _mensagemErro = "Preencha o E-mail utilizando @";
+        });
+      }
+
+    }else{
+      setState(() {
+        _mensagemErro = "Preencha o Nome";
+      });
+    }
+
+  }
+
+  _cadastrarUsuario(){
+
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,71 +69,89 @@ class _CadastroState extends State<Cadastro> {
         padding: EdgeInsets.all(16),
         child: Center(
           child: SingleChildScrollView(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(bottom: 32),
-                child: Image.asset(
-                  "images/usuario.png",
-                  width: 200,
-                  height: 150,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(bottom: 32),
+                  child: Image.asset(
+                    "imagens/usuario.png",
+                    width: 200,
+                    height: 150,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 8),
-                child: TextField(
-                  autofocus: true,
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: TextField(
+                    controller: _controllerNome,
+                    autofocus: true,
+                    keyboardType: TextInputType.text,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "Nome",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32))),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: TextField(
+                    controller: _controllerEmail,
+                    keyboardType: TextInputType.emailAddress,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                        hintText: "E-mail",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32))),
+                  ),
+                ),
+                TextField(
+                  controller: _controllerSenha,
+                  obscureText: true,
                   keyboardType: TextInputType.text,
                   style: TextStyle(fontSize: 20),
                   decoration: InputDecoration(
                       contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                      hintText: "Nome",
+                      hintText: "Senha",
                       filled: true,
                       fillColor: Colors.white,
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(32))),
                 ),
-              ), Padding(
-                padding: EdgeInsets.only(bottom: 8),
-                child: TextField(
-                  keyboardType: TextInputType.text,
-                  style: TextStyle(fontSize: 20),
-                  decoration: InputDecoration(
-                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                      hintText: "E-mail",
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(32))),
+                Padding(
+                  padding: EdgeInsets.only(top: 16, bottom: 10),
+                  child: RaisedButton(
+                      child: Text(
+                        "Cadastrar",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      color: Colors.orange,
+                      padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(32)),
+                      onPressed: () {
+                        _validarCampos();
+                      }
+                  ),
                 ),
-              ),
-              TextField(
-                keyboardType: TextInputType.text,
-                style: TextStyle(fontSize: 20),
-                decoration: InputDecoration(
-                    contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                    hintText: "Senha",
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(32))),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 16, bottom: 10),
-                child: RaisedButton(
-                  child: Text("Cadastrar",
-                      style: TextStyle(color: Colors.white, fontSize: 20)),
-                  color: Colors.orange,
-                  padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(32)),
-                  onPressed: () {},
-                ),
-              ),
-
-            ],
-          )),
+                Center(
+                  child: Text(
+                    _mensagemErro,
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 20
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
