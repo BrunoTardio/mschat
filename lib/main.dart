89c8aab61b19ebfdd2f86a8e1c945bb28c2fc24f
Bruno Tardio <brunotardiosi@gmail.com>
2020-04-02 15:23:11 -0300

    Tela Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import "package:cloud_firestore/cloud_firestore.dart";
 import 'Home.dart';
+import 'Login.dart';
 
 void main(){
   runApp(MaterialApp(
-    home: Home(),
+    home: Login(),
+    theme: ThemeData(
+      primaryColor: Colors.amber,
+      accentColor: Colors.amberAccent
+    ),
     debugShowCheckedModeBanner: false,
   ));
   
