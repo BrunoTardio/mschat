9e5b4cb13faaf17e8df6162d6c19dc06e8ce8271
Bruno Tardio <brunotardiosi@gmail.com>
2020-04-06 22:14:51 -0300

    Listando conversas

--- lib/telas/AbaConversas.dart
+++ lib/telas/AbaConversas.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mschat/model/Conversa.dart';
 
@@ -10,10 +14,14 @@ class AbaConversas extends StatefulWidget {
 class _AbaConversasState extends State<AbaConversas> {
 
   List<Conversa> _listaConversas = List();
+  final _controller = StreamController<QuerySnapshot>.broadcast();
+  Firestore db = Firestore.instance;
+  String _idUsuarioLogado;
 
   @override
   void initState() {
     super.initState();
+    _recuperarDadosUsuario();
 
     Conversa conversa = Conversa();
     conversa.nome = "Ana Clara";
@@ -24,38 +32,115 @@ class _AbaConversasState extends State<AbaConversas> {
 
   }
 
+  Stream<QuerySnapshot> _adicionarListenerConversas(){
+
+    final stream = db.collection("conversas")
+        .document( _idUsuarioLogado )
+        .collection("ultima_conversa")
+        .snapshots();
+
+    stream.listen((dados){
+      _controller.add( dados );
+    });
+
+  }
+
+  _recuperarDadosUsuario() async {
+
+    FirebaseAuth auth = FirebaseAuth.instance;
+    FirebaseUser usuarioLogado = await auth.currentUser();
+    _idUsuarioLogado = usuarioLogado.uid;
+
+    _adicionarListenerConversas();
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: _listaConversas.length,
-        itemBuilder: (context, indice){
 
-          Conversa conversa = _listaConversas[indice];
-
-          return ListTile(
-            contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
-            leading: CircleAvatar(
-              maxRadius: 30,
-              backgroundColor: Colors.grey,
-              backgroundImage: NetworkImage( conversa.caminhoFoto ),
-            ),
-            title: Text(
-              conversa.nome,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16
+    return StreamBuilder<QuerySnapshot>(
+      stream: _controller.stream,
+      builder: (context, snapshot){
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  Text("Carregando conversas"),
+                  CircularProgressIndicator()
+                ],
               ),
-            ),
-            subtitle: Text(
-                conversa.mensagem,
-                style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 14
-                )
-            ),
-          );
+            );
+            break;
+          case ConnectionState.active:
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              return Text("Erro ao carregar os dados!");
+            }else{
 
+              QuerySnapshot querySnapshot = snapshot.data;
+
+              if( querySnapshot.documents.length == 0 ){
+                return Center(
+                  child: Text(
+                    "Você não tem nenhuma mensagem ainda :( ",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                );
+              }
+
+              return ListView.builder(
+                  itemCount: _listaConversas.length,
+                  itemBuilder: (context, indice){
+
+                    List<DocumentSnapshot> conversas = querySnapshot.documents.toList();
+                    DocumentSnapshot item = conversas[indice];
+
+                    String urlImagem  = item["caminhoFoto"];
+                    String tipo       = item["tipoMensagem"];
+                    String mensagem   = item["mensagem"];
+                    String nome       = item["nome"];
+
+                    return ListTile(
+                      contentPadding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+                      leading: CircleAvatar(
+                        maxRadius: 30,
+                        backgroundColor: Colors.grey,
+                        backgroundImage: urlImagem!=null
+                            ? NetworkImage( urlImagem )
+                            : null,
+                      ),
+                      title: Text(
+                        nome,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16
+                        ),
+                      ),
+                      subtitle: Text(
+                          tipo=="texto"
+                              ? mensagem
+                              : "Imagem...",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 14
+                          )
+                      ),
+                    );
+
+                  }
+              );
+
+            }
         }
+      },
     );
+
+
   }
 }
